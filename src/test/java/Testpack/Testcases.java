08bb2dc352360ee14f24b5b2868ecbb7d30f41f9
Vidhya <vidhyamuthuraman@yahoo.co.in>
2023-12-07 21:46:35 -0600

    First Commit

---
+++ src/test/java/Testpack/Testcases.java
@@ -0,0 +1,14 @@
+package Testpack;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Testcases {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
